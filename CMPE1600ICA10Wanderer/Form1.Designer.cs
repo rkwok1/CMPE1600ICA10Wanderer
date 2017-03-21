dc5ff0cbe8713730db55a75bf21fbb0f3c48dc1a
Ryan Kwok <rkwok2@studentmail.nait.ca>
2017-03-21 14:13:12 -0600

    Created Skeleton

--- CMPE1600ICA10Wanderer/Form1.Designer.cs
+++ CMPE1600ICA10Wanderer/Form1.Designer.cs
@@ -29,11 +29,82 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.UI_TrackBar_NumberOfIterations = new System.Windows.Forms.TrackBar();
+            this.UI_FixedLabel_Min = new System.Windows.Forms.Label();
+            this.UI_FixedLabel_Max = new System.Windows.Forms.Label();
+            this.UI_FixedLabel_NumberOfIterations = new System.Windows.Forms.Label();
+            this.UI_Timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.UI_TrackBar_NumberOfIterations)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UI_TrackBar_NumberOfIterations
+            // 
+            this.UI_TrackBar_NumberOfIterations.Location = new System.Drawing.Point(12, 26);
+            this.UI_TrackBar_NumberOfIterations.Maximum = 5000;
+            this.UI_TrackBar_NumberOfIterations.Minimum = 100;
+            this.UI_TrackBar_NumberOfIterations.Name = "UI_TrackBar_NumberOfIterations";
+            this.UI_TrackBar_NumberOfIterations.Size = new System.Drawing.Size(274, 45);
+            this.UI_TrackBar_NumberOfIterations.TabIndex = 0;
+            this.UI_TrackBar_NumberOfIterations.TickFrequency = 100;
+            this.UI_TrackBar_NumberOfIterations.Value = 100;
+            this.UI_TrackBar_NumberOfIterations.Scroll += new System.EventHandler(this.UI_TrackBar_NumberOfIterations_Scroll);
+            // 
+            // UI_FixedLabel_Min
+            // 
+            this.UI_FixedLabel_Min.AutoSize = true;
+            this.UI_FixedLabel_Min.Location = new System.Drawing.Point(9, 58);
+            this.UI_FixedLabel_Min.Name = "UI_FixedLabel_Min";
+            this.UI_FixedLabel_Min.Size = new System.Drawing.Size(25, 13);
+            this.UI_FixedLabel_Min.TabIndex = 1;
+            this.UI_FixedLabel_Min.Text = "100";
+            // 
+            // UI_FixedLabel_Max
+            // 
+            this.UI_FixedLabel_Max.AutoSize = true;
+            this.UI_FixedLabel_Max.Location = new System.Drawing.Point(255, 58);
+            this.UI_FixedLabel_Max.Name = "UI_FixedLabel_Max";
+            this.UI_FixedLabel_Max.Size = new System.Drawing.Size(31, 13);
+            this.UI_FixedLabel_Max.TabIndex = 2;
+            this.UI_FixedLabel_Max.Text = "5000";
+            // 
+            // UI_FixedLabel_NumberOfIterations
+            // 
+            this.UI_FixedLabel_NumberOfIterations.AutoSize = true;
+            this.UI_FixedLabel_NumberOfIterations.Location = new System.Drawing.Point(97, 58);
+            this.UI_FixedLabel_NumberOfIterations.Name = "UI_FixedLabel_NumberOfIterations";
+            this.UI_FixedLabel_NumberOfIterations.Size = new System.Drawing.Size(102, 13);
+            this.UI_FixedLabel_NumberOfIterations.TabIndex = 3;
+            this.UI_FixedLabel_NumberOfIterations.Text = "Number of Iterations";
+            // 
+            // UI_Timer1
+            // 
+            this.UI_Timer1.Interval = 20;
+            this.UI_Timer1.Tick += new System.EventHandler(this.UI_Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 93);
+            this.Controls.Add(this.UI_FixedLabel_NumberOfIterations);
+            this.Controls.Add(this.UI_FixedLabel_Max);
+            this.Controls.Add(this.UI_FixedLabel_Min);
+            this.Controls.Add(this.UI_TrackBar_NumberOfIterations);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.UI_TrackBar_NumberOfIterations)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar UI_TrackBar_NumberOfIterations;
+        private System.Windows.Forms.Label UI_FixedLabel_Min;
+        private System.Windows.Forms.Label UI_FixedLabel_Max;
+        private System.Windows.Forms.Label UI_FixedLabel_NumberOfIterations;
+        private System.Windows.Forms.Timer UI_Timer1;
     }
 }
 
--- CMPE1600ICA10Wanderer/Form1.cs
+++ CMPE1600ICA10Wanderer/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GDIDrawer;
+using System.IO;
 
 namespace CMPE1600ICA10Wanderer
 {
     public partial class Form1 : Form
     {
+        //Global Variables
+        CDrawer drawSpace = new CDrawer(800,600,false);
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //If user adjust the track bar for iteration adjustments
+        private void UI_TrackBar_NumberOfIterations_Scroll(object sender, EventArgs e)
+        {
+            
+        }
+        //When Timer tick occurs
+        private void UI_Timer1_Tick(object sender, EventArgs e)
+        {
+            Point coor;
+            //Periodically check mouse in drawer window
+            if(drawSpace.GetLastMouseLeftClickScaled( out coor))
+            {
+                //Start point for drawing
+            }
+
         }
     }
 }
